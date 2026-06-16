8cc341bbce4e25b2f9d41d36b5594abf76aa5271
Govind Singh <govindsinghtanwar@Govinds-Mac.local>
2026-06-16 10:35:53 +0530

    fix: trigger environment fixed

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from '@trigger.dev/sdk/v3';
+import { additionalPackages } from '@trigger.dev/build/extensions/core';
 
 export default defineConfig({
   project: 'proj_ywtxnwhsofvjvekvtdrt',  // from trigger.dev dashboard
+  build: {
+    extensions: [
+      additionalPackages({
+        packages: ['@ffmpeg-installer/ffmpeg', '@ffprobe-installer/ffprobe'],
+      }),
+    ],
+  },
   logLevel: 'log',
   maxDuration: 3600,      // max 1 hour per task
   retries: {
@@ -15,3 +23,4 @@ export default defineConfig({
   },
   dirs: ['./src/trigger'],   // where your tasks live
 });
+
